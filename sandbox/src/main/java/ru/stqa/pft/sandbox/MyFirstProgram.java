e09569bf25114d9727ea263e2ea48d389b5b1f1e
eduardangel <ed_angel74@bk.ru>
2025-07-21 20:35:53 +0700

    Типы переменных

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,10 +4,11 @@ public class MyFirstProgram {
 
   public static void main (String[] args) {
 
-    System.out.println("Hello, World!");
+    String somebody = "World";
+    System.out.println("Hello, " + somebody + "!");
 
-    int l=8;
-    int s=l*l;
+    double l=8.0;
+    double s= l*l;
     System.out.println("Площадь квадрата со стороной " + l + " = " + s);
   
   }
