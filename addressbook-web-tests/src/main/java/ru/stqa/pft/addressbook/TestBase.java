60353b4b4fc73bcc38693ae028fa90100414e1e2
eduardangel <ed_angel74@bk.ru>
2025-11-22 18:36:48 +0700

    Update TestBase.java
    Создал вспомогательные функции init и stop для описания процессов Before и After. Затем, сначала extends AplicationManager, через Pull Members Up и перенес в него все имеющиеся вспомогательные функции. А потом делегировал этому классу, через Replace Inheritance with Delegations все вспомогательные функции , через app.

--- addressbook-web-tests/src/main/java/ru/stqa/pft/addressbook/TestBase.java
+++ addressbook-web-tests/src/main/java/ru/stqa/pft/addressbook/TestBase.java
@@ -2,72 +2,19 @@ package ru.stqa.pft.addressbook;
 
 import org.junit.After;
 import org.junit.Before;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class TestBase {
-    JavascriptExecutor js;
-    private WebDriver driver;
-    private Map<String, Object> vars;
+
+    protected final ApplicationManager app = new ApplicationManager();
 
     @Before
     public void setUp() {
-      driver = new FirefoxDriver();
-      js = (JavascriptExecutor) driver;
-      vars = new HashMap<String, Object>();
-      driver.get("http://localhost/addressbook/");
-      login("admin", "secret");
+        app.init();
     }
 
-    private void login(String username, String password) {
-      driver.manage().window().setSize(new Dimension(2147, 1180));
-      driver.findElement(By.name("user")).click();
-      driver.findElement(By.name("user")).sendKeys(username);
-      driver.findElement(By.name("pass")).click();
-      driver.findElement(By.name("pass")).sendKeys(password);
-      driver.findElement(By.cssSelector("input:nth-child(7)")).click();
-    }
-
-    protected void returnToGroupPage() {
-      driver.findElement(By.linkText("group page")).click();
-    }
-
-    protected void submitGroupCreation() {
-      driver.findElement(By.name("submit")).click();
-    }
-
-    protected void fillGroupForm(GroupData groupData) {
-      driver.findElement(By.name("group_name")).click();
-      driver.findElement(By.name("group_name")).sendKeys(groupData.name());
-      driver.findElement(By.name("group_header")).click();
-      driver.findElement(By.name("group_header")).sendKeys(groupData.header());
-      driver.findElement(By.name("group_footer")).click();
-      driver.findElement(By.name("group_footer")).sendKeys(groupData.footer());
-    }
-
-    protected void initGroupCreation() {
-      driver.findElement(By.name("new")).click();
-    }
-
-    protected void gotoGroupPage() {
-      driver.findElement(By.linkText("groups")).click();
-    }
-
-    protected void deleteSelectedGroups() {
-      driver.findElement(By.name("delete")).click();
-    }
-
-    protected void selectGroup() {
-      driver.findElement(By.name("selected[]")).click();
-    }
     @After
     public void tearDown() {
-        driver.quit();
+        app.stop();
     }
+
 }
