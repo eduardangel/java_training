025787f778a9981f0187d89609b7de4588ca6da5
eduardangel <ed_angel74@bk.ru>
2025-11-22 18:40:01 +0700

    Create ApplicationManager.java
    Создал новый класс, через сначала наследование , а потом делегирование, куда перенес все вспомогательные функции из Test Base.

---
+++ addressbook-web-tests/src/main/java/ru/stqa/pft/addressbook/ApplicationManager.java
@@ -0,0 +1,70 @@
+package ru.stqa.pft.addressbook;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ApplicationManager {
+    JavascriptExecutor js;
+    private WebDriver driver;
+    private Map<String, Object> vars;
+
+    protected void init() {
+        driver = new FirefoxDriver();
+        js = (JavascriptExecutor) driver;
+        vars = new HashMap<String, Object>();
+        driver.get("http://localhost/addressbook/");
+        login("admin", "secret");
+    }
+
+    private void login(String username, String password) {
+      driver.manage().window().setSize(new Dimension(2147, 1180));
+      driver.findElement(By.name("user")).click();
+      driver.findElement(By.name("user")).sendKeys(username);
+      driver.findElement(By.name("pass")).click();
+      driver.findElement(By.name("pass")).sendKeys(password);
+      driver.findElement(By.cssSelector("input:nth-child(7)")).click();
+    }
+
+    protected void returnToGroupPage() {
+      driver.findElement(By.linkText("group page")).click();
+    }
+
+    protected void submitGroupCreation() {
+      driver.findElement(By.name("submit")).click();
+    }
+
+    protected void fillGroupForm(GroupData groupData) {
+      driver.findElement(By.name("group_name")).click();
+      driver.findElement(By.name("group_name")).sendKeys(groupData.name());
+      driver.findElement(By.name("group_header")).click();
+      driver.findElement(By.name("group_header")).sendKeys(groupData.header());
+      driver.findElement(By.name("group_footer")).click();
+      driver.findElement(By.name("group_footer")).sendKeys(groupData.footer());
+    }
+
+    protected void initGroupCreation() {
+      driver.findElement(By.name("new")).click();
+    }
+
+    protected void gotoGroupPage() {
+      driver.findElement(By.linkText("groups")).click();
+    }
+
+    protected void deleteSelectedGroups() {
+      driver.findElement(By.name("delete")).click();
+    }
+
+    protected void selectGroup() {
+      driver.findElement(By.name("selected[]")).click();
+    }
+
+    protected void stop() {
+        driver.quit();
+    }
+}
