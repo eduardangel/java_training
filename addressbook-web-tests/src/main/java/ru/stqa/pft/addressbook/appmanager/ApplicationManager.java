2448754b2d8ae2ccdfeea5d3c8a3e3ee9fddcb18
eduardangel <ed_angel74@bk.ru>
2025-11-25 09:31:58 +0700

    Вернул WebDriver обратно в ApplicationManager, добавил driver в метод init, создал конструктор и вернул обращение к своему драйверу  внутри класса

--- addressbook-web-tests/src/main/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/main/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -3,6 +3,7 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.HashMap;
@@ -10,31 +11,34 @@ import java.util.Map;
 
 public class ApplicationManager {
 
-    private final NavigationHelper navigationHelper = new NavigationHelper();
+    protected WebDriver driver;
+
+    private  NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
     JavascriptExecutor js;
     private Map<String, Object> vars;
 
     public void init() {
-        navigationHelper.driver = new FirefoxDriver();
-        js = (JavascriptExecutor) navigationHelper.driver;
+        driver = new FirefoxDriver();
+        js = (JavascriptExecutor) driver;
         vars = new HashMap<String, Object>();
-        navigationHelper.driver.get("http://localhost/addressbook/");
-        groupHelper = new GroupHelper(navigationHelper.driver);
+        driver.get("http://localhost/addressbook/");
+        groupHelper = new GroupHelper(driver);
+        navigationHelper = new NavigationHelper(driver);
         login("admin", "secret");
     }
 
     private void login(String username, String password) {
-      navigationHelper.driver.manage().window().setSize(new Dimension(2147, 1180));
-      navigationHelper.driver.findElement(By.name("user")).click();
-      navigationHelper.driver.findElement(By.name("user")).sendKeys(username);
-      navigationHelper.driver.findElement(By.name("pass")).click();
-      navigationHelper.driver.findElement(By.name("pass")).sendKeys(password);
-      navigationHelper.driver.findElement(By.cssSelector("input:nth-child(7)")).click();
+      driver.manage().window().setSize(new Dimension(2147, 1180));
+      driver.findElement(By.name("user")).click();
+      driver.findElement(By.name("user")).sendKeys(username);
+      driver.findElement(By.name("pass")).click();
+      driver.findElement(By.name("pass")).sendKeys(password);
+      driver.findElement(By.cssSelector("input:nth-child(7)")).click();
     }
 
     public void stop() {
-        navigationHelper.driver.quit();
+        driver.quit();
     }
 
     public GroupHelper getGroupHelper() {
--- addressbook-web-tests/src/main/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/main/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -4,7 +4,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class NavigationHelper {
-    protected WebDriver driver;
+    private WebDriver driver;
+
+    public NavigationHelper(WebDriver driver) {
+        this.driver = driver;
+    }
 
     public void gotoGroupPage() {
       driver.findElement(By.linkText("groups")).click();
