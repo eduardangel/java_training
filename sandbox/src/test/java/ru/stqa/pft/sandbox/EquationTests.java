0f7e24036b715d18a38355193697eb4155bdb523
eduardangel <ed_angel74@bk.ru>
2025-11-30 14:24:21 +0700

    Перенес тестовые сценарии в пакет "test"

--- sandbox/src/test/java/ru/stqa/pft/sandbox/EquationTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/EquationTests.java
@@ -1,24 +1,25 @@
 package ru.stqa.pft.sandbox;
 
-import org.testng.Assert;
-import org.testng.annotations.Test;
+
+import org.junit.Assert;
+import org.junit.Test;
 
 public class EquationTests {
 
     @Test
     public void test0() {
         Equation e = new Equation(1, 1, 1);
-        Assert.assertEquals(e.rootNumber(), 0);
+        Assert.assertEquals(0, e.rootNumber());
     }
 
     @Test
     public void test1() {
         Equation e = new Equation(1, 2, 1);
-        Assert.assertEquals(e.rootNumber(), 1);
+        Assert.assertEquals(1, e.rootNumber());
     }
     @Test
     public void test2() {
         Equation e = new Equation(1, 5, 6);
-        Assert.assertEquals(e.rootNumber(), 2);
+        Assert.assertEquals(2, e.rootNumber());
     }
 }
---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/SquareConsTests.java
@@ -0,0 +1,13 @@
+package ru.stqa.pft.sandbox;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SquareConsTests {
+
+    @Test
+    public void testArea(){
+        SquareCons s = new SquareCons(5);
+        Assert.assertEquals(25, s.area(),0);
+    }
+}
