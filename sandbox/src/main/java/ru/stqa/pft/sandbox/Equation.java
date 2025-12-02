8e4ef547693cab75647e00a8fc441091f427b875
eduardangel <ed_angel74@bk.ru>
2025-12-02 16:44:34 +0700

    Изменил конструкцию "if" с равной вложенностью от else.

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Equation.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Equation.java
@@ -8,24 +8,24 @@ public class Equation {
 
     private int n;
 
-    public Equation(double a, double b, double c){
+    public Equation(double a, double b, double c) {
         this.a = a;
         this.b = b;
         this.c = c;
 
-        double d = b*b - 4*a*c;
+        double d = b * b - 4 * a * c;
 
         if (d > 0) {
             n = 2;
+        } else if (d == 0) {
+            n = 1;
         } else {
-            if (d == 0){
-                n = 1;
-            }else {
-                n = 0;
-            }
+            n = 0;
         }
+
     }
-    public int rootNumber(){
+
+    public int rootNumber() {
         return n;
     }
 }
