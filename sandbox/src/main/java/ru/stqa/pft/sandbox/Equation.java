09992916e6091df9ffd4b142e3c558407daa1ce1
eduardangel <ed_angel74@bk.ru>
2025-12-03 18:35:27 +0700

    Отформатировал код от зависимости конструкции "else if", для лучшей читаемости.

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Equation.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Equation.java
@@ -23,17 +23,16 @@ public class Equation {
             } else {
                 n = 0;
             }
-        } else {
-            if (b == 0) {
-                if (c == 0) {
-                    n = -1;
-                } else {
-                    n = 0;
-                }
+        } else if (b == 0) {
+            if (c == 0) {
+                n = -1;
             } else {
-                n = 1;
+                n = 0;
             }
+        } else {
+            n = 1;
         }
+
 
     }
 
