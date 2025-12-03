3aa6569cd3475d74e7aff1fc7ca69b4e45e829fa
eduardangel <ed_angel74@bk.ru>
2025-12-03 20:12:46 +0700

    Изменил код, с рассчетом зависимости от "c" не равной нулю для однообразия кода и лучшей читаемости.

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Equation.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Equation.java
@@ -23,12 +23,15 @@ public class Equation {
             } else {
                 n = 0;
             }
+
         } else if (b != 0) {
             n = 1;
-        } else if (c == 0) {
-            n = -1;
-        } else {
+
+        } else if (c != 0) {
             n = 0;
+
+        } else {
+            n = -1;
         }
     }
 
