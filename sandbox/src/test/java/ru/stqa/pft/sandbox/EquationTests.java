81c321ca95f20b138418b35b930e2a40772fe621
eduardangel <ed_angel74@bk.ru>
2025-12-03 18:22:30 +0700

    Добавил проверки по анализу коэффищиентов "a", "b", "c", если они == 0.

--- sandbox/src/test/java/ru/stqa/pft/sandbox/EquationTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/EquationTests.java
@@ -22,4 +22,19 @@ public class EquationTests {
         Equation e = new Equation(1, 5, 6);
         Assert.assertEquals(2, e.rootNumber());
     }
+    @Test
+    public void testLinear() {
+        Equation e = new Equation(0, 1, 1);
+        Assert.assertEquals(1, e.rootNumber());
+    }
+    @Test
+    public void testConstant() {
+        Equation e = new Equation(0, 0, 1);
+        Assert.assertEquals(0, e.rootNumber());
+    }
+    @Test
+    public void testZero() {
+        Equation e = new Equation(0, 0, 0);
+        Assert.assertEquals(-1, e.rootNumber());
+    }
 }
