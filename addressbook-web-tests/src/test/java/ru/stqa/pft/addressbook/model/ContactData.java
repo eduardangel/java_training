318b888e2278ee38f80fc89463a176e43afe89f1
eduardangel <ed_angel74@bk.ru>
2025-12-16 22:00:38 +0700

    Создал объект для заполнения формы контактов.

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -0,0 +1,5 @@
+package ru.stqa.pft.addressbook.model;
+
+public record ContactData(String firstname, String middlename, String lastname, String nickname, String title,
+                          String company, String address, String mobile, String email, String address2) {
+}
