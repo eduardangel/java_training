0320dcb0a9d52145c67c78d8574198a124902dcb
eduardangel <ed_angel74@bk.ru>
2025-12-19 11:37:00 +0700

    Преобразовал код, упростив , при помощт type и click.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -7,43 +7,31 @@ import ru.stqa.pft.addressbook.model.ContactData;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-public class ContactHelper {
-
-    private final WebDriver driver;
+public class ContactHelper extends HelperBase{
 
     public ContactHelper(WebDriver driver) {
-        this.driver = driver;
+        super(driver);
     }
 
     public void returnToHomePage() {
-      driver.findElement(By.linkText("home page")).click();
+        click(By.linkText("home page"));
     }
 
     public void submitContactForm() {
-      driver.findElement(By.cssSelector("input:nth-child(87)")).click();
+        click(By.cssSelector("input:nth-child(87)"));
     }
 
     public void fillContactForm(ContactData contactData) {
-      driver.findElement(By.name("firstname")).click();
-      driver.findElement(By.name("firstname")).sendKeys(contactData.firstname());
-      driver.findElement(By.name("middlename")).click();
-      driver.findElement(By.name("middlename")).sendKeys(contactData.middlename());
-      driver.findElement(By.name("lastname")).click();
-      driver.findElement(By.name("lastname")).sendKeys(contactData.lastname());
-      driver.findElement(By.name("nickname")).click();
-      driver.findElement(By.name("nickname")).sendKeys(contactData.nickname());
-      driver.findElement(By.name("title")).click();
-      driver.findElement(By.name("title")).sendKeys(contactData.title());
-      driver.findElement(By.name("company")).click();
-      driver.findElement(By.name("company")).sendKeys(contactData.company());
-      driver.findElement(By.name("address")).click();
-      driver.findElement(By.name("address")).sendKeys(contactData.address());
-      driver.findElement(By.name("mobile")).click();
-      driver.findElement(By.name("mobile")).sendKeys(contactData.mobile());
-      driver.findElement(By.name("email")).click();
-      driver.findElement(By.name("email")).sendKeys(contactData.email());
-      driver.findElement(By.name("address2")).click();
-      driver.findElement(By.name("address2")).sendKeys(contactData.address2());
+        type(By.name("firstname"), contactData.firstname());
+        type(By.name("middlename"), contactData.middlename());
+        type(By.name("lastname"), contactData.lastname());
+        type(By.name("nickname"), contactData.nickname());
+        type(By.name("title"), contactData.title());
+        type(By.name("company"), contactData.company());
+        type(By.name("address"), contactData.address());
+        type(By.name("mobile"), contactData.mobile());
+        type(By.name("email"), contactData.email());
+        type(By.name("address2"), contactData.address2());
     }
 
     public void acceptSelectContact() {
@@ -55,10 +43,10 @@ public class ContactHelper {
     }
 
     public void inputDeleteContact() {
-        driver.findElement(By.cssSelector(".left:nth-child(8) > input")).click();
+        click(By.cssSelector(".left:nth-child(8) > input"));
     }
 
     public void selectedContact() {
-        driver.findElement(By.name("selected[]")).click();
+        click(By.name("selected[]"));
     }
 }
